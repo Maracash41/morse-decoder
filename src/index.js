7b4a6efb44e58fb20d65b9ea12fe7677a06245d3
Maracash41 <9531945@gmail.com>
2023-10-26 18:47:39 +0300

    feat: Task is done!

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let result = '';
+  for(let i = 0; i < expr.length; i += 10) {
+    let char = expr.substring(i, i + 10)
+    if (char !== '**********') {
+        char = char.replace(/11/g, '-');
+        char = char.replace(/10/g, '.');
+        char = char.replace(/0/g, '');
+        console.log(char)
+        result +=  MORSE_TABLE[char];
+    }
+    else {
+        result += ' ';
+    }
+  }
+  return result;
 }
 
 module.exports = {
